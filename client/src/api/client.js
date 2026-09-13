fb8536b06f9c19fc8e9c932ffed637f1982ed269
lightningX <darkalpha621@gmail.com>
2026-09-13 12:05:49 +0530

    Update client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -38,6 +38,10 @@ export function login({ email, password }) {
   return request('/auth/login', { method: 'POST', body: { email, password } });
 }
 
+export function googleAuth(credential) {
+  return request('/auth/google', { method: 'POST', body: { credential } });
+}
+
 export function getConversations(token) {
   return request('/conversations', { token });
 }
